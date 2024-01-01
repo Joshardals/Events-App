fb949fc0ca1ec4865e393745bb9c1357ba7b3c61
Joshardals <joshuabamidele219@gmail.com>
2024-01-01 16:36:04 +0100

    initial commit

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+"use client";
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavItems() {
+  const pathname = usePathname();
   return (
-    <div>NavItems</div>
-  )
+    <ul className="flex md:flex-between w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link, index) => {
+        const isActive = pathname === link.route;
+        return (
+          <li
+            key={index}
+            className={`${
+              isActive && "text-primary-500"
+            } flex-center p-medium-16 whitespace-nowrap`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
