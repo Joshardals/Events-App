1eaf4ce1b6d373f17da68652ed85bb684bc36b2c
Joshardals <joshuabamidele219@gmail.com>
2024-01-02 12:52:07 +0100

    initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold capitalize">
+              host, connect, celebrate: your events, our platform
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book and learn helpful tips from 3.168+ mentors in world-class
+              companies with our global community.
+            </p>
+            <Button asChild size="lg" className="button w-full">
+              <Link href="#events">Explore Now</Link>
+            </Button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
