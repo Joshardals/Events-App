166f8a25e2acb6c77667516be62e955fb86c632c
Joshardals <joshuabamidele219@gmail.com>
2024-01-05 16:03:50 +0100

    initial commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,43 +1,43 @@
-// import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-// import Image from "next/image";
-// import Link from "next/link";
-// import { Button } from "../ui/button";
-// import NavItems from "./NavItems";
-// import MobileNav from "./MobileNav";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
-// export default function Header() {
-//   return (
-//     <header className="w-full border-b">
-//       <div className="wrapper flex items-center justify-between">
-//         <Link href="/" className="w-36">
-//           <Image
-//             src="/assets/images/logo.svg"
-//             width={128}
-//             height={38}
-//             alt="Evently Logo"
-//           />
-//         </Link>
+export default function Header() {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently Logo"
+          />
+        </Link>
 
-//         <SignedIn>
-//           <nav className="md:flex-between hidden w-full max-w-xs">
-//             <NavItems />
-//           </nav>
-//         </SignedIn>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
 
-//         <div className="flex w-32 justify-end gap-3">
-//           <SignedIn>
-//             <div className="flex items-center space-x-2">
-//               <UserButton afterSignOutUrl="/" />
-//               <MobileNav />
-//             </div>
-//           </SignedIn>
-//           <SignedOut>
-//             <Button asChild className="rounded-full" size="lg">
-//               <Link href="/sign-in">Login</Link>
-//             </Button>
-//           </SignedOut>
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <div className="flex items-center space-x-2">
+              <UserButton afterSignOutUrl="/" />
+              <MobileNav />
+            </div>
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+}
