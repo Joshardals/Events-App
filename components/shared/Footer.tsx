6f826645d07b11a84e0a8e69ac323291a4784d69
Joshardals <joshuabamidele219@gmail.com>
2024-01-05 16:03:25 +0100

    initial commit

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,24 +1,24 @@
-// import Image from "next/image";
-// import Link from "next/link";
+import Image from "next/image";
+import Link from "next/link";
 
-// export default function Footer() {
-//   return (
-//     <footer className="border-t">
-//       <div
-//         className="flex-center wrapper flex-between flex flex-col 
-//       gap-4 p-5 text-center sm:flex-row"
-//       >
-//         <Link href="/">
-//           <Image
-//             src="/assets/images/logo.svg"
-//             alt="logo"
-//             width={138}
-//             height={38}
-//           />
-//         </Link>
+export default function Footer() {
+  return (
+    <footer className="border-t">
+      <div
+        className="flex-center wrapper flex-between flex flex-col 
+      gap-4 p-5 text-center sm:flex-row"
+      >
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={138}
+            height={38}
+          />
+        </Link>
 
-//         <p>2024 Evently. All Rights reserved.</p>
-//       </div>
-//     </footer>
-//   );
-// }
+        <p>2024 Evently. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
